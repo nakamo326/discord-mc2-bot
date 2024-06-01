6f7c11fb077b866d1fbad29935980f69cde91b27
Ynakamot <nakamo326@gmail.com>
2024-06-01 17:50:54 +0900

    ビルドスクリプトを追加

---
+++ esbuild.mjs
@@ -0,0 +1,17 @@
+import * as esbuild from "esbuild";
+import * as glob from "glob";
+
+const entryPoints = glob.sync("./src/*").map((file) => `./${file}`);
+console.log(entryPoints);
+
+await esbuild.build({
+  entryPoints: entryPoints,
+  entryNames: "[dir]/[name]/index",
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  platform: "node",
+  target: ["node18"],
+  external: ["aws-sdk"],
+  outdir: "dist",
+});
