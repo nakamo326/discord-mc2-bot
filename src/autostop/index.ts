81678480a6cd43143666d25013652e895902db11
Ynakamot <nakamo326@gmail.com>
2024-12-22 15:09:28 +0900

    fix: 関数名を修正

--- src/autostop/index.ts
+++ src/autostop/index.ts
@@ -2,14 +2,14 @@ import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 
 const lambdaClient = new LambdaClient({});
 
-const statusServerfunctionARN = process.env.STATUS_SERVER_INSTANCES_LAMBDA;
-const stopEC2functionARN = process.env.STOP_EC2_INSTANCES_LAMBDA;
+const statusServerFunctionARN = process.env.STATUS_SERVER_INSTANCES_LAMBDA;
+const stopEC2FunctionARN = process.env.STOP_EC2_INSTANCES_LAMBDA;
 
 // check if the server is running, and online player is 0, then stop the server
 exports.handler = async (event) => {
   try {
     const command = new InvokeCommand({
-      FunctionName: statusServerfunctionARN,
+      FunctionName: statusServerFunctionARN,
       InvocationType: "RequestResponse",
     });
 
@@ -20,7 +20,7 @@ exports.handler = async (event) => {
 
     if (serverStatus.online && serverStatus.online === true && serverStatus.onlinePlayerNum === 0) {
       const stopCommand = new InvokeCommand({
-        FunctionName: stopEC2functionARN,
+        FunctionName: stopEC2FunctionARN,
         InvocationType: "Event",
       });
 
